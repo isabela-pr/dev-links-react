14091d90c43eea412951bde1bafcffa9c6c2211a
isabela-pr <icerqueira440@gmail.com>
2023-08-11 11:05:38 -0300

    switch button OK

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,11 +1,11 @@
+import { useState } from "react";
 import Profile from "../components/profile/profile";
 
 import "./App.css";
 
-import { useState } from "react";
-import imgDarkMode from "../img/fotoPerfil.jpeg"
-import imgLightMode from "../img/fotoPerfil2.jpeg"
-
+import imgDarkMode from "../img/fotoPerfil.jpeg";
+import imgLightMode from "../img/fotoPerfil2.jpeg";
+import Switch from "../components/switch/switch";
 
 const App = () => {
 const [isLightMode, setIsLightMode] = useState(false)
@@ -13,8 +13,9 @@ const [isLightMode, setIsLightMode] = useState(false)
 const imgSrc = isLightMode ? imgLightMode : imgDarkMode;
 const imgAlt = isLightMode ? "Imagem no Modo Claro" : "Imagem no Modo Escuro"
     return(
-        <div>
+        <div id="container">
           <Profile imgSrc={imgSrc} imgAlt={imgAlt}>@isabela-pr</Profile>
+          <Switch></Switch>
         </div>
     )
 }
